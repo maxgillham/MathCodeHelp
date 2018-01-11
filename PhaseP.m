39f76f707ac25a98423b7b23391b20128d5143e3
maxgillham <14mfg2@queensu.ca>
2018-01-11 13:03:44 -0500

    source code

--- PhaseP.m
+++ PhaseP.m
@@ -1,10 +1,11 @@
 
-x = linspace(-2,2,14);
-y = linspace(-2,2,14);
 
 [X,Y] = meshgrid(x,y);
-
+%function here
 U = 4.*X - 1.*Y; % dx/dt
 V = 4.*X; % dy/dt
+
+x = linspace(-2,2,14);
+y = linspace(-2,2,14);
 
 quiver(X,Y,U,V)
